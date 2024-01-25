4f8b7ca84563e87d463b6857d1bfa0271a33db2a
aditya pandit <adityapanditdev@gmail.com>
2024-01-25 18:42:19 +0530

    Spec for left and right rotation

--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -24,5 +24,35 @@ RSpec.describe Spacecraft do
         expect(spacecraft.direction).to eq('N')
       end
     end
+    
+    context 'rotation' do
+      it 'should turns left from North to West' do
+        spacecraft = Spacecraft.new
+        spacecraft.execute_commands(['l'])
+        expect(spacecraft.position).to eq([0, 0, 0])
+        expect(spacecraft.direction).to eq('W')
+      end
+
+      it 'should turns right from North to East' do
+        spacecraft = Spacecraft.new
+        spacecraft.execute_commands(['r'])
+        expect(spacecraft.position).to eq([0, 0, 0])
+        expect(spacecraft.direction).to eq('E')
+      end
+
+      it 'should turns left multiple times' do
+        spacecraft = Spacecraft.new
+        spacecraft.execute_commands(['l', 'l'])
+        expect(spacecraft.position).to eq([0, 0, 0])
+        expect(spacecraft.direction).to eq('S')
+      end
+
+      it 'should turns right multiple times' do
+        spacecraft = Spacecraft.new
+        spacecraft.execute_commands(['r', 'r', 'r'])
+        expect(spacecraft.position).to eq([0, 0, 0])
+        expect(spacecraft.direction).to eq('W')
+      end
+    end
   end
 end
