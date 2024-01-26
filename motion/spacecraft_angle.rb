632f8496046c495628b3c41773cd078280c82880
aditya pandit <adityapanditdev@gmail.com>
2024-01-26 14:53:44 +0530

    Create spacecraft angle

---
+++ motion/spacecraft_angle.rb
@@ -0,0 +1,11 @@
+module SpacecraftAngle
+  def turn_upward(direction)
+    val = 'Up'
+    [val, direction]
+  end
+
+  def turn_downward(direction)
+    val = 'Down'
+    [val, direction]
+  end
+end
